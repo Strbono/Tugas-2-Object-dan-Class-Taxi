6a2d5fbbc0a19633b83401ff2d3c185180092da9
satrio <wibicrull35@gmail.com>
2021-03-17 22:51:51 +0700

    Object dan Class taxi

---
+++ 02-Taxi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Taxi
+{
+    class Program
+    {
+        //pembuatan objek taxi
+        Taxi taxi = new Taxi();
+        static void Main(string[] args)
+        {
+        }
+    }
+}
